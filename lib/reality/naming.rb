e692702966e68b263d3463487bf1fe743f5f875c
Peter Donald <peter@realityforge.org>
2016-11-30 21:44:44 +1100

    Use class << self syntax

--- lib/reality/naming.rb
+++ lib/reality/naming.rb
@@ -14,97 +14,100 @@
 
 module Reality
   module Naming
-    def self.camelize?(word)
-      camelize(word) == word
-    end
 
-    def self.camelize(input_word)
-      word_parts = split_into_words(input_word).collect{|part| part[0...1].upcase + part[1..-1]}
-      return word_parts[0].downcase if (word_parts.size == 1 && word_parts[0] == word_parts[0].upcase)
-      word = word_parts.join('')
-      word = word[0...1].downcase + word[1..-1]
-      word
-    end
-
-    def self.pascal_case?(word)
-      pascal_case(word) == word
-    end
-
-    def self.pascal_case(input_word)
-      word_parts = split_into_words(input_word).collect{|part| part[0...1].upcase + part[1..-1]}
-      return word_parts[0] if (word_parts.size == 1 && word_parts[0] == word_parts[0].upcase)
-      word = word_parts.join('')
-      word
-    end
-
-    def self.underscore?(word)
-      underscore(word) == word
-    end
-
-    def self.underscore(input_word)
-      word = split_into_words(input_word).join('_')
-      word.downcase!
-      word
-    end
-
-    def self.xmlize?(word)
-      xmlize(word) == word
-    end
-
-    def self.xmlize(camel_cased_word)
-      underscore(camel_cased_word).tr('_', '-')
-    end
-
-    def self.jsonize?(word)
-      jsonize(word) == word
-    end
-
-    def self.jsonize(camel_cased_word)
-      camelize(camel_cased_word)
-    end
-
-    def self.uppercase_constantize?(word)
-      uppercase_constantize(word) == word
-    end
-
-    def self.uppercase_constantize(camel_cased_word)
-      underscore(camel_cased_word).upcase
-    end
-
-    def self.pluralize(string)
-      singular = string.to_s
-      length = singular.size
-      last_ch = last(singular)
-      last_2ch = last(singular, 2)
-
-      plural = nil
-      if last_ch == 'y'
-        plural = "#{singular[0, length - 1]}ies" unless singular =~ /[aeiou]y$/
-      elsif last_ch == 'f'
-        plural = "#{singular[0, length - 1]}ves" unless singular =~ /[aeiou][aeiou]f$/
-      elsif last_2ch == 'fe'
-        plural = "#{singular[0, length - 2]}ves"
-      elsif %w(ch sh).include?(last_2ch)
-        plural = "#{singular[0, length - 2]}es"
-      elsif %w(s x z).include?(last_ch)
-        plural = "#{singular[0, length - 1]}es"
+    class << self
+      def camelize?(word)
+        camelize(word) == word
       end
-      plural || "#{singular}s"
-    end
 
-    private
+      def camelize(input_word)
+        word_parts = split_into_words(input_word).collect { |part| part[0...1].upcase + part[1..-1] }
+        return word_parts[0].downcase if (word_parts.size == 1 && word_parts[0] == word_parts[0].upcase)
+        word = word_parts.join('')
+        word = word[0...1].downcase + word[1..-1]
+        word
+      end
 
-    def self.split_into_words(camel_cased_word)
-      word = camel_cased_word.to_s.dup
-      word.gsub!(/([A-Z]+)([A-Z][a-z])/, '\1_\2')
-      word.gsub!(/([a-z\d])([A-Z])/, '\1_\2')
-      word.tr!('-', '_')
-      word.split('_')
-    end
+      def pascal_case?(word)
+        pascal_case(word) == word
+      end
 
-    def self.last(s, limit = 1)
-      return s if limit > s.size
-      s[-limit, limit]
+      def pascal_case(input_word)
+        word_parts = split_into_words(input_word).collect { |part| part[0...1].upcase + part[1..-1] }
+        return word_parts[0] if (word_parts.size == 1 && word_parts[0] == word_parts[0].upcase)
+        word = word_parts.join('')
+        word
+      end
+
+      def underscore?(word)
+        underscore(word) == word
+      end
+
+      def underscore(input_word)
+        word = split_into_words(input_word).join('_')
+        word.downcase!
+        word
+      end
+
+      def xmlize?(word)
+        xmlize(word) == word
+      end
+
+      def xmlize(camel_cased_word)
+        underscore(camel_cased_word).tr('_', '-')
+      end
+
+      def jsonize?(word)
+        jsonize(word) == word
+      end
+
+      def jsonize(camel_cased_word)
+        camelize(camel_cased_word)
+      end
+
+      def uppercase_constantize?(word)
+        uppercase_constantize(word) == word
+      end
+
+      def uppercase_constantize(camel_cased_word)
+        underscore(camel_cased_word).upcase
+      end
+
+      def pluralize(string)
+        singular = string.to_s
+        length = singular.size
+        last_ch = last(singular)
+        last_2ch = last(singular, 2)
+
+        plural = nil
+        if last_ch == 'y'
+          plural = "#{singular[0, length - 1]}ies" unless singular =~ /[aeiou]y$/
+        elsif last_ch == 'f'
+          plural = "#{singular[0, length - 1]}ves" unless singular =~ /[aeiou][aeiou]f$/
+        elsif last_2ch == 'fe'
+          plural = "#{singular[0, length - 2]}ves"
+        elsif %w(ch sh).include?(last_2ch)
+          plural = "#{singular[0, length - 2]}es"
+        elsif %w(s x z).include?(last_ch)
+          plural = "#{singular[0, length - 1]}es"
+        end
+        plural || "#{singular}s"
+      end
+
+      private
+
+      def split_into_words(camel_cased_word)
+        word = camel_cased_word.to_s.dup
+        word.gsub!(/([A-Z]+)([A-Z][a-z])/, '\1_\2')
+        word.gsub!(/([a-z\d])([A-Z])/, '\1_\2')
+        word.tr!('-', '_')
+        word.split('_')
+      end
+
+      def last(s, limit = 1)
+        return s if limit > s.size
+        s[-limit, limit]
+      end
     end
   end
 end
